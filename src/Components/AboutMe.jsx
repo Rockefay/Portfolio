d0fe2cd3e0e936e6ff5c24809425670cb25d54e1
Krzysztof <kr.purgat@gmail.com>
2022-09-13 14:13:26 +0200

    Design landing page

---
+++ src/Components/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div className="AboutMe">
+      <h2>About Me</h2>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <div className="container">
+        <div className="content">
+          <h1 className="hello">Hello</h1>
+          <h2 className="heading">I'm Krzysztof</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
