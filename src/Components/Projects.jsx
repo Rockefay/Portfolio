4ab3bbe41146ba30af0fbbfb804ebf81525acc56
Krzysztof <kr.purgat@gmail.com>
2022-09-18 12:11:12 +0200

    Add second project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fillOpacity="1"
-          d="M0,288L60,282.7C120,277,240,267,360,261.3C480,256,600,256,720,261.3C840,267,960,277,1080,256C1200,235,1320,181,1380,154.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          d="M0,128L80,138.7C160,149,320,171,480,154.7C640,139,800,85,960,58.7C1120,32,1280,32,1360,32L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg>
       <div className="container">
@@ -32,7 +32,58 @@ function Projects() {
             </div>
           </div>
         </div>
+
+        <div className="content">
+          <div className="text">
+            <h3>Advent of JavaScript</h3>
+            <div>
+              The project contains 24 columns. Each challenge includes HTMS and
+              CSS files and the main task is to write JavaScript code to give
+              functionality to the site.
+            </div>
+          </div>
+          <div className="images">
+            <img src="Images\adventofjs.jpg" alt="" />
+          </div>
+          <div className="text">Some of the challanges:</div>
+          <div className="gallery">
+            <div className="item">
+              <img src="Images\Budget.png" alt="" />
+              <div>Simple budget calculator</div>
+            </div>
+            <div className="item">
+              <img src="Images\Weather.png" alt="" />
+              <div>Weather app using an API</div>
+            </div>
+            <div className="item">
+              <img src="Images\Dynamic_Table.png" alt="" />
+              <div>Table of data with sorting and editing</div>
+            </div>
+            <div className="item">
+              <img src="Images\Food_Order.png" alt="" />
+              <div>Food Order app</div>
+            </div>
+            <div className="item">
+              <img src="Images\Form_Validator.png" alt="" />
+              <div>Form Validator</div>
+            </div>
+            <div className="item">
+              <img src="Images\Password_Generator.png" alt="" />
+              <div>Password Generator</div>
+            </div>
+            <div className="item">
+              <img src="Images\Rock_Paper_Scissors1.png" alt="" />
+              <div>Rock, Paper, Scissors game</div>
+            </div>
+          </div>
+        </div>
       </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fillOpacity="1"
+          d="M0,128L80,138.7C160,149,320,171,480,154.7C640,139,800,85,960,58.7C1120,32,1280,32,1360,32L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+        ></path>
+      </svg>
     </div>
   );
 }
