6a5da996bc8dbf3a802bb14156848a211bf19c4d
Krzysztof <kr.purgat@gmail.com>
2022-09-20 12:32:41 +0200

    Add carousels

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,19 +1,9 @@
-import React, { useState } from "react";
-import { cardGameImages } from "../consts";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { adventProjects, cardGameImages } from "../consts";
 
 function Projects() {
-  const [imageIndex, setImageIndex] = useState(0);
-
-  const changeImage = (direction) => {
-    if (direction === "forward") {
-      if (imageIndex < cardGameImages.length - 1) setImageIndex(imageIndex + 1);
-      else setImageIndex(0);
-    } else {
-      if (imageIndex > 0) setImageIndex(imageIndex - 1);
-      else setImageIndex(cardGameImages.length - 1);
-    }
-  };
-
   return (
     <div className="Projects">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -34,25 +24,21 @@ function Projects() {
             </div>
           </div>
           <div className="images">
-            <img src={cardGameImages[imageIndex]} alt="" />
-            <div className="arows">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                onClick={() => changeImage("back")}
-              >
-                <path d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"></path>
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                onClick={() => changeImage("forward")}
-              >
-                <path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"></path>
-              </svg>
-            </div>
+            <Carousel
+              showStatus={false}
+              showThumbs={false}
+              swipeable={true}
+              emulateTouch={true}
+              className="carousel"
+            >
+              {cardGameImages.map((image) => {
+                return (
+                  <div key={image}>
+                    <img src={image} alt="" />
+                  </div>
+                );
+              })}
+            </Carousel>
           </div>
         </div>
 
@@ -60,9 +46,10 @@ function Projects() {
           <div className="text">
             <h3>Advent of JavaScript</h3>
             <div>
-              The project contains 24 columns. Each challenge includes HTMS and
-              CSS files and the main task is to write JavaScript code to give
-              functionality to the site.
+              The project contains 24 challenges. Each challenge includes HTMS
+              and CSS files and the main task is to write JavaScript code to
+              give functionality to the site. Link to the project:&nbsp;
+              <a href="https://www.adventofjs.com/">www.adventofjs.com</a>
             </div>
           </div>
           <div className="images">
@@ -71,34 +58,22 @@ function Projects() {
           <div className="text">Some of the challanges:</div>
         </div>
         <div className="gallery">
-          <div className="item">
-            <img src="Images\Budget.png" alt="" />
-            <div>Simple budget calculator</div>
-          </div>
-          <div className="item">
-            <img src="Images\Weather.png" alt="" />
-            <div>Weather app using an API</div>
-          </div>
-          <div className="item">
-            <img src="Images\Dynamic_Table.png" alt="" />
-            <div>Table of data with sorting and editing</div>
-          </div>
-          <div className="item">
-            <img src="Images\Food_Order.png" alt="" />
-            <div>Food Order app</div>
-          </div>
-          <div className="item">
-            <img src="Images\Form_Validator.png" alt="" />
-            <div>Form Validator</div>
-          </div>
-          <div className="item">
-            <img src="Images\Password_Generator.png" alt="" />
-            <div>Password Generator</div>
-          </div>
-          <div className="item">
-            <img src="Images\Rock_Paper_Scissors1.png" alt="" />
-            <div>Rock, Paper, Scissors game</div>
-          </div>
+          <Carousel
+            showStatus={false}
+            showThumbs={false}
+            swipeable={true}
+            emulateTouch={true}
+            className="carousel"
+          >
+            {adventProjects.map((project) => {
+              return (
+                <div className="item" key={project.src}>
+                  <img src={project.src} alt="" />
+                  <div>{project.description}</div>
+                </div>
+              );
+            })}
+          </Carousel>
         </div>
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
--- src/consts.js
+++ src/consts.js
@@ -3,3 +3,34 @@ export const cardGameImages = [
   "Images\\Card_Game2.png",
   "Images\\Card_Game3.png",
 ];
+
+export const adventProjects = [
+  {
+    src: "Images\\Budget.png",
+    description: "Simple budget calculator",
+  },
+  {
+    src: "Images\\Weather.png",
+    description: "Weather app using an API",
+  },
+  {
+    src: "Images\\Dynamic_Table.png",
+    description: "Table of data with sorting and editing",
+  },
+  {
+    src: "Images\\Food_Order.png",
+    description: "Food Order app",
+  },
+  {
+    src: "Images\\Form_Validator.png",
+    description: "Form Validator",
+  },
+  {
+    src: "Images\\Password_Generator.png",
+    description: "Password Generator",
+  },
+  {
+    src: "Images\\Rock_Paper_Scissors1.png",
+    description: "Rock, Paper, Scissors game",
+  },
+];
