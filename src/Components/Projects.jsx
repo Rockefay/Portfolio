90c770e3679fffe499c3cb0f8d9bf83b75751b6b
Krzysztof <kr.purgat@gmail.com>
2023-03-06 18:03:49 +0100

    Add new project and fix icons

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,8 +1,9 @@
 import "aos/dist/aos.css";
 import React from "react";
+import { SiGithub } from "react-icons/si";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { adventProjects, cardGameImages } from "../consts";
+import { adventProjects, cardGameImages, ebHelperImages } from "../consts";
 import "./Projects.scss";
 
 function Projects() {
@@ -16,7 +17,41 @@ function Projects() {
       </svg>
       <div className="container">
         <h2>Projects</h2>
+
         <div className="content project1" data-aos="fade-up">
+          <div className="text">
+            <h3>Eurobusiness Helper</h3>
+            <div>
+              Cooperative project created to help while playing the board game Eurobusiness. The
+              application helps with transactions between players, bank and themselves. As a member
+              responsible for front-end, I created the layout of the application as well as build
+              the interface that users see and interact with.
+            </div>
+            <div className="button">
+              <SiGithub />
+              <a href="https://github.com/Purgaty/EurobusinessHelper">Code on GitHub</a>
+            </div>
+          </div>
+          <div className="images">
+            <Carousel
+              showStatus={false}
+              showThumbs={false}
+              swipeable={true}
+              emulateTouch={true}
+              className="carousel"
+            >
+              {ebHelperImages.map((image) => {
+                return (
+                  <div key={image}>
+                    <img src={image} alt={image} />
+                  </div>
+                );
+              })}
+            </Carousel>
+          </div>
+        </div>
+
+        <div className="content project2" data-aos="fade-up">
           <div className="text">
             <h3>Card Game</h3>
             <div>
@@ -24,13 +59,7 @@ function Projects() {
               Solitaire and compare your score with other players.
             </div>
             <div className="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
-                ></path>
-              </svg>
+              <SiGithub />
               <a href="https://github.com/Rockefay/Card-Game">Code on GitHub</a>
             </div>
           </div>
@@ -45,7 +74,7 @@ function Projects() {
               {cardGameImages.map((image) => {
                 return (
                   <div key={image}>
-                    <img src={image} alt="" />
+                    <img src={image} alt={image} />
                   </div>
                 );
               })}
@@ -53,7 +82,7 @@ function Projects() {
           </div>
         </div>
 
-        <div className="content project2" data-aos="fade-up">
+        <div className="content project3" data-aos="fade-up">
           <div className="text">
             <h3>Advent of JavaScript</h3>
             <div>
@@ -63,18 +92,12 @@ function Projects() {
               <a href="https://www.adventofjs.com/">www.adventofjs.com</a>
             </div>
             <div className="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
-                ></path>
-              </svg>
+              <SiGithub />
               <a href="https://github.com/Rockefay/Advent-of-JavaScript">Code on GitHub</a>
             </div>
           </div>
           <div className="images">
-            <img src="Images\adventofjs.jpg" alt="" />
+            <img src="Images\adventofjs.jpg" alt="Images\adventofjs.jpg" />
           </div>
           <div className="text">Some of the challanges:</div>
           <div className="gallery">
@@ -88,7 +111,7 @@ function Projects() {
               {adventProjects.map((project) => {
                 return (
                   <div className="item" key={project.src}>
-                    <img src={project.src} alt="" />
+                    <img src={project.src} alt={project.src} />
                     <div>{project.description}</div>
                   </div>
                 );
